5932a77cd243853443216a6a33027014e3cb5b6f
RayfoRen <50767211+RayfoRen@users.noreply.github.com>
2019-06-21 11:40:25 +0200

    ilde - zombie

---
+++ Assets/GameAssets/scripts/MovimientoSeguimiento.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoSeguimiento : MonoBehaviour {
+
+
+    [SerializeField] Transform jugador;
+    [SerializeField] float velocidadMovimiento = 5;
+    CharacterController miCC;
+
+    private void Awake()
+    {
+        miCC = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        this.transform.LookAt(jugador);
+        miCC.SimpleMove(this.transform.forward * velocidadMovimiento);
+    }
+
+}
